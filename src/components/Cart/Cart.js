7075f73208e1a2fa9731f3b77c424e3bc295b232
JunayetTamim <junayettamim186@gmail.com>
2022-03-26 02:27:01 +0600

    code update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,24 +1,23 @@
 import './Cart.css'
 
 const Cart = ({cart}) => {
-    // console.log(cart);
-     /* let name = [];
-      for(const product of cart){
-         name = [...name,product.name];
-     }  */ 
+
+    const chooseBtn = () =>{
+        console.log('clicked');
+    }
     return (
         <div className='cart'>
             <h2>Selected Flowers</h2>
             <div className='flower-list'>
             {
                 cart.map(item => 
+                    
                     <h4>{item.name}</h4>
                 )
                 
             }
-                {/*  <h2 className='Selected-item'>{name}</h2> */} 
             </div>
-            <button>CHOOSE 1 FOR ME</button>
+            <button onClick={chooseBtn}>CHOOSE 1 FOR ME</button>
             <button>CHOOSE AGAIN</button>
         </div>
     );
--- src/components/Flower/Flower.js
+++ src/components/Flower/Flower.js
@@ -4,12 +4,13 @@ import React from 'react';
 import './Flower.css'
 
 const Flower = ({flower, handleAddToCart}) => {
-    const {name, img} = flower;
+    const {name, img, price} = flower;
     return (
         <div className='flower'>
             <img src={img} alt="" />
             <div className='flower-info'>
-                <h2>Flower Name: {name}</h2>
+                <h2>Flower Name: <small>{name}</small></h2>
+                <h2>Price: <small>{price}</small></h2>
             </div>
             <button  onClick={() =>handleAddToCart(flower)} className='btn-cart'>
                <p className='btn-text'>Add to Cart</p>
